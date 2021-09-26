aa218027ede6ddbd79d3a01f967701db4281e074
artemkoochoogoora <89087882+artemkoochoogoora@users.noreply.github.com>
2021-09-26 22:00:47 +0300

    bomb with New Sound

--- countdown.py
+++ countdown.py
@@ -6,6 +6,6 @@ for number in range(10,0000,-1):
     print (number, "\a")
     sleep(1)
 print ("бомба взорвалась" )
-playsound.playsound('00562.mp3', True)
+playsound.playsound('zvuk-vzryvy-zvuk-vzryva-v-tveri-21_11_12-i-signalizaciy-avtomobili.mp3', True)
 
 
